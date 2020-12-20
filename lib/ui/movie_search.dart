389af37ea2baa70d5211aeb6b482234fdf4e1a57
Tekeshwar Singh <tekeshwarsingh2000@gmail.com>
2020-12-20 17:21:51 +0530

    search page structure

--- lib/ui/movie_search.dart
+++ lib/ui/movie_search.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+
 class MovieSearch extends StatefulWidget {
   @override
   _MovieSearchState createState() => _MovieSearchState();
@@ -23,7 +25,7 @@ class _MovieSearchState extends State<MovieSearch> {
           keyboardType: TextInputType.text,
           textInputAction: TextInputAction.search,
           onEditingComplete: () {
-            FocusScope.of(context).requestFocus(FocusNode());
+            // FocusScope.of(context).requestFocus(FocusNode());
           },
         ),
         actions: <Widget>[
@@ -32,14 +34,79 @@ class _MovieSearchState extends State<MovieSearch> {
             child: IconButton(
               alignment: Alignment.centerRight,
               onPressed: () {
-                FocusScope.of(context).requestFocus(FocusNode());
+                // FocusScope.of(context).requestFocus(FocusNode());
               },
               icon: Icon(Icons.search),
             ),
           ),
         ],
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+        child: ListView.separated(
+          separatorBuilder: (_, __) => Divider(),
+          itemCount: 5,
+          itemBuilder: (BuildContext context, int index) {
+            return Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.pink,
+                  ),
+                  height: MediaQuery.of(context).size.height * 0.25,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width / 3.4,
+                      ),
+                      Expanded(
+                        child: ListTile(
+                          title: Text("Tenet",
+                              style: Theme.of(context).textTheme.headline5),
+                          subtitle: Row(
+                            children: ['advenure', 'family', 'cation']
+                                .map((e) => Chip(label: Text('$e')))
+                                .toList(),
+                          ),
+                          trailing: Container(
+                            padding: EdgeInsets.all(15),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12.0),
+                              color: Colors.amber,
+                            ),
+                            child: Text(
+                              "5.0",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  height: MediaQuery.of(context).size.height * 0.25,
+                  width: MediaQuery.of(context).size.width / 3.5,
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    // image: DecorationImage(
+                    //   image: NetworkImage("$posterURL"),
+                    //   fit: BoxFit.cover,
+                    // ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
